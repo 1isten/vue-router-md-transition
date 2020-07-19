0c1bb8e86102e16a85658c36e3b9e334ff753e06
1isten <sten.li@foxmail.com>
2020-07-19 22:55:06 +0800

    minor improvements

--- src/assets/MaterialDesignTransition.js
+++ src/assets/MaterialDesignTransition.js
@@ -1,4 +1,4 @@
-export default (props = {}) => {
+export default function (props = {}) {
   if (!props.offsetTop) props.offsetTop = 64;
   if (!props.duration) props.duration = 250;
   props.reverse = !!props.reverse;
@@ -22,83 +22,94 @@ export default (props = {}) => {
   computed.isDisabled = props.disabled || computed.duration0;
 
   const methods = {
-    beforeEnter(b) {
-      if (computed.isDisabled) return;
-
-      data.b = b;
-      const bStyle = getComputedStyle(b);
-      data.bStyleVars = '';
-      data.bStyleVars += `--transform-original: ${bStyle.transform}; `;
-      data.bStyleVars += `--opacity-original: ${bStyle.opacity}; `;
-      data.bStyleVars += `--duration: ${props.duration}ms; `;
-      data.bStyleVars += `--transform: translate3d(0, ${props.offsetTop}px, 0);`;
-      b.style.cssText += data.bStyleVars;
-      b.classList.add('md-transition-hidden'); // hide b before a's width, height, margin, etc., are computed
-    },
-    beforeLeave(a) {
-      if (computed.isDisabled) return;
-
-      data.a = a;
-      const aStyle = getComputedStyle(a);
-      data.aStyleVars = '';
-      data.aStyleVars += `--width-a: ${aStyle.width}; `;
-      data.aStyleVars += `--height-a: ${aStyle.height}; `;
-      data.aStyleVars += `--margin-a: ${aStyle.marginTop} ${aStyle.marginRight} ${aStyle.marginBottom} ${aStyle.marginLeft}; `;
-      data.aStyleVars += `--transform-original: ${aStyle.transform}; `;
-      data.aStyleVars += `--opacity-original: ${aStyle.opacity}; `;
-      data.aStyleVars += `--duration: ${props.duration}ms; `;
-      data.aStyleVars += `--transform: translate3d(0, ${props.offsetTop}px, 0);`;
-      a.style.cssText += data.aStyleVars;
-      if (data.b) data.b.classList.remove('md-transition-hidden'); // unhide b after a's width, height, margin, etc., are computed
-    },
-
-    leave(a, done) {
-      if (computed.isDisabled) return done();
-
-      a.addEventListener('animationend', done, { once: true });
-      a.classList.add(`md-transition-${computed.direction}-leave-active`);
-    },
-    enter(b, done) {
-      if (computed.isDisabled) return done();
-
-      b.addEventListener('animationend', done, { once: true });
-      b.classList.add(`md-transition-${computed.direction}-enter-active`);
-    },
-
-    afterEnter(b) {
-      if (computed.isDisabled) return;
-
-      b.classList.remove(`md-transition-${computed.direction}-enter-active`);
-      b.setAttribute('style', b.style.cssText.replace(data.bStyleVars, '').trim());
-      if (!b.getAttribute('style')) b.removeAttribute('style');
-    },
-    afterLeave(a) {
-      if (computed.isDisabled) return;
-
-      a.classList.remove(`md-transition-${computed.direction}-leave-active`);
-      a.setAttribute('style', a.style.cssText.replace(data.aStyleVars, '').trim());
-      if (!a.getAttribute('style')) a.removeAttribute('style');
-    },
-
-    enterCancelled(b) {
-      if (computed.isDisabled) return;
-
-      b.classList.remove('md-transition-forward-enter-active');
-      b.classList.remove('md-transition-backward-enter-active');
-      b.setAttribute('style', b.style.cssText.replace(data.bStyleVars, '').trim());
-      if (!b.getAttribute('style')) b.removeAttribute('style');
-      // clearTimeout(window.__VUE_MD_TRANSITION_SCROLL_TIMEOUT__);
-    },
-    leaveCancelled(a) {
-      if (computed.isDisabled) return;
-
-      a.classList.remove('md-transition-forward-enter-active');
-      a.classList.remove('md-transition-backward-enter-active');
-      a.setAttribute('style', a.style.cssText.replace(data.aStyleVars, '').trim());
-      if (!a.getAttribute('style')) a.removeAttribute('style');
-      // clearTimeout(window.__VUE_MD_TRANSITION_SCROLL_TIMEOUT__);
-    },
+    beforeEnter,
+    beforeLeave,
+    leave,
+    enter,
+    afterEnter,
+    afterLeave,
+    enterCancelled,
+    leaveCancelled,
   };
+  function beforeEnter(b) {
+    if (computed.isDisabled) return;
+
+    data.b = b;
+    const bStyle = getComputedStyle(b);
+    data.bStyleVars = '';
+    data.bStyleVars += `--transform-original: ${bStyle.transform}; `;
+    data.bStyleVars += `--opacity-original: ${bStyle.opacity}; `;
+    data.bStyleVars += `--duration: ${props.duration}ms; `;
+    data.bStyleVars += `--transform: translate3d(0, ${props.offsetTop}px, 0);`;
+    b.style.cssText += data.bStyleVars;
+    b.classList.add('md-transition-hidden'); // hide b before a's width, height, margin, etc., are computed
+  }
+  function beforeLeave(a) {
+    if (computed.isDisabled) return;
+
+    data.a = a;
+    const aStyle = getComputedStyle(a);
+    data.aStyleVars = '';
+    data.aStyleVars += `--width-a: ${aStyle.width}; `;
+    data.aStyleVars += `--height-a: ${aStyle.height}; `;
+    data.aStyleVars += `--margin-a: ${aStyle.marginTop} ${aStyle.marginRight} ${aStyle.marginBottom} ${aStyle.marginLeft}; `;
+    data.aStyleVars += `--transform-original: ${aStyle.transform}; `;
+    data.aStyleVars += `--opacity-original: ${aStyle.opacity}; `;
+    data.aStyleVars += `--duration: ${props.duration}ms; `;
+    data.aStyleVars += `--transform: translate3d(0, ${props.offsetTop}px, 0);`;
+    a.style.cssText += data.aStyleVars;
+    if (data.b) data.b.classList.remove('md-transition-hidden'); // unhide b after a's width, height, margin, etc., are computed
+  }
+  function leave(a, done) {
+    if (computed.isDisabled) return done();
+
+    a.addEventListener('animationend', function () {
+      done();
+      afterLeave(a);
+    }, { once: true });
+    a.classList.add(`md-transition-${computed.direction}-leave-active`);
+  }
+  function enter(b, done) {
+    if (computed.isDisabled) return done();
+
+    b.addEventListener('animationend', function () {
+      done();
+      afterEnter(b);
+    }, { once: true });
+    b.classList.add(`md-transition-${computed.direction}-enter-active`);
+  }
+  function afterEnter(b) {
+    if (computed.isDisabled) return;
+
+    b.classList.remove(`md-transition-${computed.direction}-enter-active`);
+    b.setAttribute('style', b.style.cssText.replace(data.bStyleVars, '').trim());
+    if (!b.getAttribute('style')) b.removeAttribute('style');
+  }
+  function afterLeave(a) {
+    if (computed.isDisabled) return;
+
+    a.classList.remove(`md-transition-${computed.direction}-leave-active`);
+    a.setAttribute('style', a.style.cssText.replace(data.aStyleVars, '').trim());
+    if (!a.getAttribute('style')) a.removeAttribute('style');
+  }
+  function enterCancelled(b) {
+    if (computed.isDisabled) return;
+
+    b.classList.remove('md-transition-forward-enter-active');
+    b.classList.remove('md-transition-backward-enter-active');
+    b.setAttribute('style', b.style.cssText.replace(data.bStyleVars, '').trim());
+    if (!b.getAttribute('style')) b.removeAttribute('style');
+    // clearTimeout(window.__VUE_MD_TRANSITION_SCROLL_TIMEOUT__);
+  }
+  function leaveCancelled(a) {
+    if (computed.isDisabled) return;
+
+    a.classList.remove('md-transition-forward-enter-active');
+    a.classList.remove('md-transition-backward-enter-active');
+    a.setAttribute('style', a.style.cssText.replace(data.aStyleVars, '').trim());
+    if (!a.getAttribute('style')) a.removeAttribute('style');
+    // clearTimeout(window.__VUE_MD_TRANSITION_SCROLL_TIMEOUT__);
+  }
 
   return {
     name: `md-transition-${computed.direction}`,
@@ -118,4 +129,4 @@ export default (props = {}) => {
     enterCancelled: methods.enterCancelled,
     leaveCancelled: methods.leaveCancelled,
   };
-};
+}
